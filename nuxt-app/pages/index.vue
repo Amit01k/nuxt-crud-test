0ee48ec72793bec0bc1e31fe8c54174b1c3c41f9
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-08-02 14:30:17 +0530

    bucket error

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -5,7 +5,7 @@
     <p class="text-center text-red-400 text-3xl ">Welcome to our Nuxt-crud-page</p>
     <div class="m-8 items-center ">
         <nuxt-link to="login" class="border border-gray-400 px-4 py-2 max-w-xs rounded bg-teal-600 text-white ">Login</nuxt-link>
-    <nuxt-link to="sign" class="border border-gray-400 px-4 py-2 max-w-xs rounded bg-teal-600 text-white">Sign Up</nuxt-link>
+    <!-- <nuxt-link to="sign" class="border border-gray-400 px-4 py-2 max-w-xs rounded bg-teal-600 text-white">Sign Up</nuxt-link> -->
     </div>
 
     <p class=" text-xl text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, asperiores assumenda quae culpa quidem consequuntur laborum est, iste, voluptate magnam ullam exercitationem architecto quod pariatur vitae eveniet earum molestias. Ducimus harum ratione, omnis veritatis totam quod sit quo? Iusto aspernatur dolore modi voluptate error et soluta consequatur, facilis perspiciatis non. Ipsam ex ad quod, vel ut totam ratione enim sunt.</p>
