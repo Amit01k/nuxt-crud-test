c72b1d031142b98a40ff46601617072c7715755d
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-08-04 14:10:45 +0530

    all done

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="">
+    <div class="flex justify-center mt-20">
         
-        <div class="bg-orange-400 border-spacing-1 w-2/4">
+        <div class="bg-orange-400 border-spacing-1 w-2/4 flex justify-items-center flex-col">
             <h1 class="text-2xl text-yellow-300 text-center">This is login page </h1>
-            <p class="text-center">email<input
+            <p class="text-center">Email<input
                     class="border border-gray-400 block px-4 py-1 w-1/4 rounded focus:outline-none focus:border-teal-500 mx-auto "
                     type="email" v-model="email" /></p>
-            <p class="text-center">password <input
+            <p class="text-center">Password <input
                     class="border border-gray-400 block px-4 py-1 w-1/4 rounded focus:outline-none focus:border-teal-500 mx-auto"
                     type="password" v-model="password" /></p>
             <div class="flex">
                 <div class=" flex justify-center">
                     <button @click="login"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mx-72 mt-4 mb-4 "> login</button>
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-md mx-72 mt-4 mb-4 "> login</button>
                 </div>
             </div>
         </div>
